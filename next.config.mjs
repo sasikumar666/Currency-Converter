c7ce126fab008c53cb267bec638414bface5fc4a
sasikumar666 <sasikumar2221666@gmail.com>
2026-01-14 22:23:16 +0530

    baseFileChanged

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/Currency-Converter',
+  assetPrefix: '/Currency-Converter/',
   typescript: {
     ignoreBuildErrors: true,
   },
