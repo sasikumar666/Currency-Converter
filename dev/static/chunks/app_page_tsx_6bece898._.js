6982fd702e8d2f7d151814fe4d0b5376e4e4f848
sasikumar666 <sasikumar2221666@gmail.com>
2026-01-14 22:24:16 +0530

    Updates

--- dev/static/chunks/app_page_tsx_6bece898._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_a8312267._.js",
-  "static/chunks/_9f494a7e._.js"
-],
-    source: "dynamic"
-});
